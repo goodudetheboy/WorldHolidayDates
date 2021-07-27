7b11bf3a6225a5ef05fcfc920d92060226a515ae
Vuong Ho <hochivuong2002@gmail.com>
2021-07-27 11:21:00 +0700

    Add support for easter/orthodox

--- lib/src/main/java/worldholidaydates/holidayparser/Date.java
+++ lib/src/main/java/worldholidaydates/holidayparser/Date.java
@@ -7,4 +7,28 @@ public interface Date {
     public LocalDateTime calculate();
 
     public LocalDate calculateDate();
+
+    /**
+     * Return the date that is offset by some days from the input date,
+     * according to the following rule:
+     * <ol>
+     * <li> offset < 0: return [offset] days before input date
+     * <li> offset > 0: return [offset] days after input date
+     * <li> offset = 0: return original date
+     * </ol>
+     * <p>
+     * 
+     * @param date an input date
+     * @param offset offset, in days
+     * @return the date that is offset by some [offset] days from the input date
+     */
+    public static LocalDate getOffsetDate(LocalDate date, int offset) {
+        if (offset > 0) {
+            return date.plusDays(offset);
+        } 
+        if (offset < 0) {
+            return date.minusDays(-offset);
+        }
+        return date;
+    }
 }
--- lib/src/test/java/worldholidaydates/UnitTest.java
+++ lib/src/test/java/worldholidaydates/UnitTest.java
@@ -16,19 +16,6 @@ import worldholidaydates.holidayparser.HolidayParser;
 import worldholidaydates.holidayparser.ParseException;
 
 public class UnitTest {
-    @Test
-    public void gregorianDateTest() {
-        String input = "2021-05-01";
-        LocalDateTime expected = LocalDateTime.parse("2021-05-01T00:00");
-        testParser(input, expected);
-    }
-
-    @Test
-    public void startOfMonthTest() {
-        String input = "January";
-        LocalDateTime expected = LocalDateTime.parse("2021-01-01T00:00");
-        testParser(input, expected);
-    }
 
     public void testParser(String input, LocalDateTime expected) {
         HolidayParser parser = new HolidayParser(new ByteArrayInputStream(input.getBytes()));
@@ -38,7 +25,29 @@ public class UnitTest {
             assertEquals(expected, actual);
         } catch (ParseException e) {
             e.printStackTrace();
-            fail(e.getMessage());
+            fail("Parse fail at \"" + input + "\" - " + e.getMessage());
         }
+    }
+
+    @Test
+    public void gregorianDateTest() {
+        testParser("2021-05-01", LocalDateTime.parse("2021-05-01T00:00"));
+    }
+
+    @Test
+    public void startOfMonthTest() {
+        testParser("january", LocalDateTime.parse("2021-01-01T00:00"));
+        testParser("march", LocalDateTime.parse("2021-03-01T00:00"));
+    }
+
+
+    @Test
+    public void easterOrthodoxTest() {
+        testParser("easter", LocalDateTime.parse("2021-04-04T00:00"));
+        testParser("orthodox", LocalDateTime.parse("2021-05-02T00:00"));
+        testParser("easter -2", LocalDateTime.parse("2021-04-02T00:00"));
+        testParser("orthodox 3", LocalDateTime.parse("2021-05-05T00:00"));
+        testParser("easter +49", LocalDateTime.parse("2021-05-23T00:00"));
+        testParser("orthodox -6", LocalDateTime.parse("2021-04-26T00:00"));
     }
 }
