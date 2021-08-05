8fb4b9cde0889c1057e02adce031ff0f15800e72
Vuong Ho <hochivuong2002@gmail.com>
2021-08-05 12:07:56 +0700

    Fix writing holidays to file

--- lib/src/test/java/worldholidaydates/holidaydata/DataTest.java
+++ lib/src/test/java/worldholidaydates/holidaydata/DataTest.java
@@ -1,6 +1,16 @@
 package worldholidaydates.holidaydata;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -9,5 +19,40 @@ public class DataTest {
     public void dataJsonParseTest() {
         HolidayData data = HolidayData.initializeData();
         assertEquals(168, data.getHolidays().size());
+    }
+
+    @Test
+    public void printHolidayTest() {
+        HolidayData data = HolidayData.initializeData();
+        Map<String, Country> holidays = data.getHolidays();
+        BufferedWriter outputExpected = null;
+        try {
+            outputExpected = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("holiday-data/" + "all-holidays.txt"), StandardCharsets.UTF_8));
+            for (Map.Entry<String, Country> entry : holidays.entrySet()) {
+                Country c = entry.getValue();
+                outputExpected.write(entry.getKey() + "\n");
+                if (c.getDays() != null) {
+                    for (LocalDate holiday : c.getHolidayDates()) {
+                        if (holiday != null) {
+                            outputExpected.write(holiday.toString() + "\n");
+                        }
+                    }
+                }
+            }
+            outputExpected.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        } finally {
+            try {
+                outputExpected.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                fail(e.getMessage());
+            }
+        }
     }
 }
--- lib/src/test/java/worldholidaydates/holidaydata/UnitTest.java
+++ lib/src/test/java/worldholidaydates/holidaydata/UnitTest.java
@@ -1,42 +1,4 @@
 package worldholidaydates.holidaydata;
 
-import static org.junit.Assert.fail;
-
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.util.Map;
-
-import org.junit.Test;
-
 public class UnitTest {
-    @Test
-    public void printHolidayTest() {
-        HolidayData data = HolidayData.initializeData();
-        Map<String, Country> holidays = data.getHolidays();
-        try {
-            BufferedWriter outputExpected = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("holiday-data/" + "all-holidays.txt"), StandardCharsets.UTF_8));
-            for (Map.Entry<String, Country> entry : holidays.entrySet()) {
-                Country c = entry.getValue();
-                outputExpected.write(entry.getKey() + "\n");
-                if (c.getDays() != null) {
-                    for (LocalDate holiday : c.getHolidayDates()) {
-                        if (holiday != null) {
-                            outputExpected.write(holiday.toString() + "\n");
-                        }
-                    }
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        }
-    }
 }
